d78bbdcd1ea086d6989dfeaecaa2e0a1753428c7
Chris Wacek <cwacek@gmail.com>
2013-10-06 13:58:44 -0400

    Removed old logging configuration

--- scanner/filereader/filereader.go
+++ scanner/filereader/filereader.go
@@ -1,17 +1,5 @@
 package filereader
 
-import log "github.com/cihub/seelog"
-
-func setupLogging(config_xml string) {
-  logger, err := log.LoggerFromConfigAsFile(config_xml)
-
-  if err  != nil {
-    panic(err)
-  }
-
-  log.ReplaceLogger(logger)
-}
-
 type Document interface {
   Identifier() string
   Tokens() <-chan *Token
