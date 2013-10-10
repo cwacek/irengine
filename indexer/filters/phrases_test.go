36d68502f6d94aa3aaa42e9e0e9918ee13ce2ee9
Chris Wacek <cwacek@gmail.com>
2013-10-10 00:06:57 -0400

    Functional phrase index

---
+++ indexer/filters/phrases_test.go
@@ -0,0 +1,33 @@
+package filters
+
+import "github.com/cwacek/irengine/scanner/filereader"
+
+var phrase_expected = []*filereader.Token {
+  &filereader.Token{Text: "once", Type: filereader.TextToken},
+  &filereader.Token{Text: "a young", Type: filereader.TextToken},
+  &filereader.Token{Text: "young man", Type: filereader.TextToken},
+  &filereader.Token{Text: "When", Type: filereader.TextToken},
+  &filereader.Token{Text: "played with", Type: filereader.TextToken},
+  &filereader.Token{Text: "with shoes", Type: filereader.TextToken},
+  &filereader.Token{Text: "frequently", Type: filereader.TextToken},
+  &filereader.Token{Text: "would wear", Type: filereader.TextToken},
+  &filereader.Token{Text: "wear them", Type: filereader.TextToken},
+  &filereader.Token{Text: "Tarnation", Type: filereader.TextToken},
+}
+
+var phrase_input = `I once was a young man. When I was, I played
+with shoes; frequently I would wear them. Tarnation.`
+
+func init() {
+
+    builder := func(id string) Filter {
+        return NewPhraseFilter(2, 0.4)
+    }
+
+  AddTestCase("phrases",TestCase{
+    builder,
+    phrase_input,
+    phrase_expected,
+  })
+
+}
