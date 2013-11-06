aae78efb5fa518837257baf4ccff84930545fb9a
Chris Wacek <cwacek@gmail.com>
2013-11-06 05:50:23 -0500

    Fix a bunch of things in the LM model

--- query_engine/language_model.go
+++ query_engine/language_model.go
@@ -40,6 +40,11 @@ func (lm *DirichletQL) ProcessPositional(
 			pl.Len(), index.DocumentCount))
 	}
 
+	avgDocLen := float64(index.TermCount()) / float64(index.DocumentCount)
+	if lm.mu == 0 {
+		lm.mu = math.Sqrt(avgDocLen)
+	}
+
 	//Make a fake term with the posting list
 	term = &indexer.Term{"<phrase>", -1, pl}
 
@@ -56,13 +61,13 @@ func (lm *DirichletQL) ProcessPositional(
 		partial_score = tf_d
 		log.Debugf("Tf_d %f", tf_d)
 		partial_score += lm.mu *
-			(float64(term.Tf()) / float64(index.DocumentCount))
+			(float64(term.Tf()) / float64(index.TermCount()))
 		log.Debugf("numerator: %f", partial_score)
 		partial_score /= float64(doc_info.TermCount) + lm.mu
 		log.Debugf("after division: %f. Log %f",
-			partial_score, math.Log10(partial_score))
+			partial_score, math.Log(partial_score*1000.0))
 
-		docScores[pl_entry.DocId()] += math.Log10(partial_score)
+		docScores[pl_entry.DocId()] += math.Log(partial_score * 1000)
 	}
 
 	responseSet := NewResponse()
@@ -101,7 +106,7 @@ func (lm *DirichletQL) ProcessQuery(
 
 	avgDocLen := float64(index.TermCount()) / float64(index.DocumentCount)
 	if lm.mu == 0 {
-		lm.mu = avgDocLen
+		lm.mu = math.Sqrt(avgDocLen)
 	}
 
 	var partial_score, tf_d float64
@@ -122,21 +127,30 @@ func (lm *DirichletQL) ProcessQuery(
 		pl = term.PostingList()
 		for pl_iter = pl.Iterator(); pl_iter.Next(); {
 			pl_entry = pl_iter.Value()
-			tf_d = float64(pl_entry.Frequency())
 			doc_info = index.DocumentMap[pl_entry.DocId()]
+
+			tf_d = float64(pl_entry.Frequency())
 
 			log.Debugf("Obtained PL Entry %v with frequency %f", pl_entry, tf_d)
 
 			partial_score = tf_d
 			log.Debugf("Tf_d %f", tf_d)
 			partial_score += lm.mu *
-				(float64(term.Tf()) / float64(index.DocumentCount))
+				(float64(term.Tf()) / float64(index.TermCount()))
 			log.Debugf("numerator: %f", partial_score)
+			log.Debugf("TermCount:%d, DocCount: %d", doc_info.TermCount, index.DocumentCount)
 			partial_score /= float64(doc_info.TermCount) + lm.mu
 			log.Debugf("after division: %f. Log %f",
-				partial_score, math.Log10(partial_score))
+				partial_score, math.Log(partial_score))
 
-			docScores[pl_entry.DocId()] += math.Log10(partial_score)
+			//When we take the logarithm, our numbers are so small that it ends up
+			// being negative. This has a pathological result because documents with *more*
+			// of the query terms end up worse off than docs with fewer because they add
+			// more negative numbers. Solution: multiply by 1000 before taking the log.
+
+			docScores[pl_entry.DocId()] += math.Log(partial_score * 1000.0)
+			log.Debugf("Added %f to docScore for %s. Total: %f",
+				math.Log(partial_score*1000.0), pl_entry.DocId(), docScores[pl_entry.DocId()])
 		}
 	}
 
